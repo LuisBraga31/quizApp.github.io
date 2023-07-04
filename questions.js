2cb81383ed2ea4048416a3ca408e8827286bea80
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-07-04 15:36:35 -0300

    Finalizando quiz

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Quantos países tem na África?",
+      answers: [
+        { option: "54", correto: true },
+        { option: "48", correto: false },
+        { option: "45", correto: false },
+      ],
+    },
+    {
+      question: "Qual a capital do Equador?",
+      answers: [
+        { option: "Medellin", correct: false },
+        { option: "Quito", correct: true },
+        { option: "Santiago", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o 4 país mais populoso do mundo?",
+      answers: [
+        { option: "Indonésia", correct: true },
+        { option: "Estados Unidos", correct: false },
+        { option: "Paquistão", correct: false },
+      ],
+    },
+    {
+      question: "Qual a capital da Austrália?",
+      answers: [
+        { option: "Melbourne", correct: false },
+        { option: "Sydney", correct: false },
+        { option: "Camberra", correct: true },
+      ],
+    },
+  ];
