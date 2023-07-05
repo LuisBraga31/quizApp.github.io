2a08e08525c2c4e7273610c0a2bcb7a2aed3d232
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-07-05 11:32:29 -0300

    Adicionando feedback

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ const content = document.querySelector(".content");
 const contentFinish = document.querySelector(".finish");
 const btnRestart = document.querySelector(".finish button");
 
+const feedbackContent = document.querySelector(".feedback");
+
 import questions from "./questions.js";
 
 let indexAtual = 0;
 let acertos = 0;
+let feedback = [];
+let feedback2 = [];
 
 btnRestart.onclick = () => {
-  content.style.display = "flex";
-  contentFinish.style.display = "none";
-
+  content.classList.toggle('content-off');
+  contentFinish.classList.toggle('finish-on');
+  feedback = [];
+  feedback2 = [];
+  feedbackContent.innerHTML = "";
   indexAtual = 0;
   acertos = 0;
   loadQuestion();
@@ -22,29 +28,52 @@ btnRestart.onclick = () => {
 
 function nextQuestion(e) {
   
-  if (e.target.getAttribute("data-correct")) {
+  if (e.target.getAttribute("data-correct") === "true") {
     acertos++;
+    feedback.push(true);
+  } else {
+    feedback.push(false);
   }
 
   if (indexAtual < questions.length - 1) {
     indexAtual++;
     loadQuestion();
   } else {
-    finish();
+    finalizar();
   }
 }
 
-function finish() {
+function finalizar() {
   textFinish.innerHTML = `Você acertou ${acertos} de ${questions.length}`;
-  content.style.display = "none";
-  contentFinish.style.display = "flex";
+
+  console.log(feedback);
+  console.log(feedback2);
+
+  for(let i =0; i < 4; i++) {
+    console.log(feedback[i] + " " + feedback2[i]);
+    if(feedback[i] === true) {
+      feedbackContent.innerHTML += `<p class ="green"> ${i+1}. ${feedback2[i]} </p>`;
+    } else {
+      feedbackContent.innerHTML += `<p class ="red"> ${i+1}. ${feedback2[i]} </p>`;
+    }
+    
+    
+  }
+  
+  console.log(feedbackContent);
+
+  content.classList.toggle('content-off');
+  contentFinish.classList.toggle('finish-on');
+
 }
 
 function loadQuestion() {
   spnQtd.innerHTML = `${indexAtual + 1}/${questions.length}`;
   const item = questions[indexAtual];
   answers.innerHTML = "";
+  feedback2.push(item.question);
   question.innerHTML = item.question;
+  
 
   item.answers.forEach((answer) => {
     const div = document.createElement("div");
